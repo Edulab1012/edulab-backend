b39a0aeeed2253bb3ec82f0751298aaca1549e87
Oyunbat <oyunbat9958@gmail.com>
2025-06-05 01:00:16 +0800

    delete , edit comment done

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -5,6 +5,9 @@ import {
   getPostById,
   addComment,
   getCommentsByPost,
+  updateComment,
+  deleteComment,
+  getPostsByTeacher,
 } from "../controllers/postController";
 
 const router = express.Router();
@@ -14,5 +17,9 @@ router.get("/class/:classId", getPostsByClass);
 router.get("/:postId", getPostById);
 router.post("/:postId/comments", addComment);
 router.get("/:postId/comments", getCommentsByPost);
-
+// Add these new routes
+router.put("/comments/:commentId", updateComment);
+router.delete("/comments/:commentId", deleteComment);
+// Add to your post routes
+router.get("/teacher/:teacherId", getPostsByTeacher);
 export default router;
