7898b0142d0eb74bd72aac704f4bd97c0168e51f
URSA0112 <Nimone99@gmail.com>
2025-06-06 06:32:51 +0800

    deployeee

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,34 @@ import authRoutes from "./routes/auth";
 import attendanceRoutes from "./routes/attendance";
 // import teacherRoutes from "./routes/teacher";
 import studentRoutes from "./routes/student";
-import classRoutes from "./routes/class";
-import { createServer } from "http";
+import classesRoutes from "./routes/classes";
+
+
 import { createClient } from "@supabase/supabase-js";
 // import semesterRoutes from "./routes/semester";
-import { createClass } from "./controllers/ClassController";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const supabaseUrl = process.env.SUPABASE_URL || "your-supabase-url";
-const supabaseKey = process.env.SUPABASE_KEY || "your-supabase-key";
+const supabaseUrl = process.env.SUPABASE_URL || "https://iuuliuoqgudrqrjfdsuo.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1dWxpdW9xZ3VkcnFyamZkc3VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMzczODIsImV4cCI6MjA2MzYxMzM4Mn0.Sw85JlpgRLP8P_dXBn9Fa5rnnjHez62v85U5v1ps9KA";
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://edulab-frontend-kappa.vercel.app/"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
 app.use("/api/v1/attendance", attendanceRoutes);
 app.use("/api/v1/auth", authRoutes);
 // app.use("/api/v1/teacher", teacherRoutes);
 app.use("/api/v1/student", studentRoutes);
 app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/class", classRoutes);
+app.use("/api/v1/class", classesRoutes);
 // app.use("/api/v1/semester", semesterRoutes);
 app.get("/", (req: Request, res: Response) => {
   try {
